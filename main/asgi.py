87be08b01cfa72203ff03c3059c463be95102379
Matt Bertrand <mrbertrand@gmail.com>
2025-01-27 11:13:24 -0500

    Fix litellm, make django admin work

--- main/asgi.py
+++ main/asgi.py
@@ -6,6 +6,7 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from django.urls import re_path
 
 from main.middleware.configs import HTTP_MIDDLEWARE
 from main.middleware.util import apply_middleware
@@ -20,7 +21,13 @@ import ai_chatbots.routing  # noqa: E402, I001
 application = ProtocolTypeRouter(
     {
         "http": apply_middleware(
-            HTTP_MIDDLEWARE, URLRouter(ai_chatbots.routing.http_patterns)
+            HTTP_MIDDLEWARE,
+            URLRouter(
+                [
+                    *ai_chatbots.routing.http_patterns,
+                    re_path(r"", django_asgi_app),
+                ]
+            ),
         ),
     }
 )
