08908d64afa42f4d87320d6924a170f6191f24b3
Doof <mitx-devops@mit.edu>
2025-03-19 14:45:52 +0000

    Release 0.3.1

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ from main.envs import (
 from main.sentry import init_sentry
 from openapi.settings_spectacular import open_spectacular_settings
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 
 log = logging.getLogger()
 
