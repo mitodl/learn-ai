0d397aff230c33160f48846c0836df86838d5c7e
shankar ambady <ambady@mit.edu>
2025-06-04 11:49:36 -0400

    Revert "fixing imports"
    This reverts commit 5396337a30fdfea7c7013e5d2149846749951ed7.

--- frontend-demo/src/services/learn/learning-resources/queries.ts
+++ frontend-demo/src/services/learn/learning-resources/queries.ts
@@ -1,13 +1,9 @@
 import { queryOptions } from "@tanstack/react-query"
-// Import the correct named export for the API class
+import { getAPISettings } from "../settings"
 import { LearningResourcesApi } from "@mitodl/mitxonline-api-axios/v1"
-// If this does not work, check the package documentation for the correct export name
-// Replace with the correct type import or define locally if not exported by the package
-// import type { LearningResourcesApiLearningResourcesRetrieveRequest } from "@mitodl/mitxonline-api-axios/v1"
-type LearningResourcesApiLearningResourcesRetrieveRequest = {
-  id: number
-}
+import type { LearningResourcesApiLearningResourcesRetrieveRequest } from "@mitodl/mitxonline-api-axios/v1"
 
+const learningResources = new LearningResourcesApi(...getAPISettings())
 const keys = {
   root: () => ["learning_resources"],
   retrieve: (opts: LearningResourcesApiLearningResourcesRetrieveRequest) => [
@@ -22,9 +18,7 @@ const queries = {
     queryOptions({
       queryKey: keys.retrieve(opts),
       queryFn: () =>
-        LearningResourcesApi.learningResourcesRetrieve(opts).then(
-          (r) => r.data,
-        ),
+        learningResources.learningResourcesRetrieve(opts).then((r) => r.data),
     }),
 }
 
