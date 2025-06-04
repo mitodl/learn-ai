fe5fec4c33c3b13ec936408412c2266e2f1c5ef0
Matt Bertrand <mattbert@mit.edu>
2025-06-04 09:28:14 -0400

    More summary prompt tweaking (#208)

--- ai_chatbots/prompts.py
+++ ai_chatbots/prompts.py
@@ -84,14 +84,18 @@ information."""
 # The following prompts are similar or identical to the default ones in
 # langmem.short_term.summarization
 PROMPT_SUMMARY_INITIAL = """Create a summary of the conversation above, incorporating
-thevprevious summary if any.
+the previous summary if any.
 If there are any tool results, include the full output of the latest tool message in
-the summary.  You must also retain all title and readable_id field values from all tool
-messages and any previous summaries in this new summary.
+the summary.  You must also retain all "title" and "readable_id" field values from all
+tool messages and any previous summaries in this new summary.
 """
 PROMPT_SUMMARY_EXISTING = """This is summary of the conversation so far:
 {existing_summary}
-\n\nExtend this summary by taking into account the new messages above:"""
+\n\nExtend this summary by taking into account the new messages above.
+If there are any tool results, include the full output of the latest tool message in
+the summary.  You must also retain all "title" and "readable_id" field values from all
+tool messages and any previous summaries in this new summary.
+"""
 PROMPT_SUMMARY_FINAL = """Summary of the conversation so far: {summary}"""
 
 
