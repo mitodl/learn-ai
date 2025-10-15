69ab1dab0468264daf2e35c923213c38b025f1c4
Matt Bertrand <mattbert@mit.edu>
2025-10-15 09:53:23 -0400

    Use granian instead of uvicorn

--- scripts/run-django-dev.sh
+++ scripts/run-django-dev.sh
@@ -13,7 +13,4 @@ python3 manage.py createcachetable
 # run ONLY data migrations
 RUN_DATA_MIGRATIONS=true python3 manage.py migrate --noinput
 
-uvicorn main.asgi:application --reload --host 0.0.0.0 --port 8001
-
-# production might best be run via a combo of gunicorn and uvicorn?
-#gunicorn main.asgi:application -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8001 --workers 4 --threads 2
+granian --interface asgi --host 0.0.0.0 --port 8001 --reload main.asgi:application
