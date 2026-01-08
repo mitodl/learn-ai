5ff2b8f10c05879ac7f71e06af54ed04ad51754c
shankar ambady <ambady@mit.edu>
2026-01-08 15:17:35 -0500

    adding search tab

--- frontend-demo/src/app/(home)/ChatTabs.tsx
+++ frontend-demo/src/app/(home)/ChatTabs.tsx
@@ -9,6 +9,7 @@ import SyllabusContent from "./SyllabusContent"
 import SyllabusCanvasContent from "./SyllabusCanvasContent"
 import AssessmentContent from "./AssessmentContent"
 import CanvasAssessmentContent from "./CanvasAssessmentContent"
+import ContentFileSearchContent from "./ContentFileSearchContent"
 import VideoContent from "./VideoContent"
 import { useEffect, useState } from "react"
 
@@ -19,6 +20,7 @@ export enum ChatTab {
   VideoGPT = "VideoGPT",
   AssessmentGPT = "AssessmentGPT",
   CanvasAssessmentGPT = "CanvasAssessmentGPT",
+  ContentFileSearch = "ContentFileSearch",
 }
 
 const ChatTabs = () => {
@@ -48,6 +50,7 @@ const ChatTabs = () => {
           <Tab label="SyllabusCanvasGPT" value={ChatTab.SyllabusCanvasGPT} />
           <Tab label="VideoGPT" value={ChatTab.VideoGPT} />
           <Tab label="AssessmentGPT" value={ChatTab.AssessmentGPT} />
+          <Tab label="ContentFileSearch" value={ChatTab.ContentFileSearch} />
           <Tab
             label="CanvasAssessmentGPT"
             value={ChatTab.CanvasAssessmentGPT}
@@ -71,6 +74,9 @@ const ChatTabs = () => {
       </TabPanel>
       <TabPanel value={ChatTab.CanvasAssessmentGPT}>
         <CanvasAssessmentContent />
+      </TabPanel>
+      <TabPanel value={ChatTab.ContentFileSearch}>
+        <ContentFileSearchContent />
       </TabPanel>
     </TabContext>
   )
